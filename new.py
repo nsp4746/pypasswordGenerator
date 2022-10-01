fbe0d4617e2e344198a6c5eefb7e9fecdd4db045
nsp4746 <nsp4746@rit.edu>
2022-10-01 14:00:15 -0400

    Alphabet intialized and created

--- new.py
+++ new.py
@@ -1,5 +1,14 @@
+# Author: Nikhil Patil
+# Python Password Generator
+# Generates Passwords
+
 import string
 import secrets
+
+LETTERS = string.ascii_letters 
+DIGITS = string.digits
+SPECIALCHARS = string.punctuation
+ALPHABET = LETTERS+DIGITS+SPECIALCHARS # Concatnate to create the alphabet
 
 def main():
     print("Hello World")
