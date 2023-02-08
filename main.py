f3d6672d30f58251bfb35661d83102662485c716
nsp4746 <nsp4746@rit.edu>
2023-02-07 20:39:45 -0500

    Created a new function, needs docs

--- main.py
+++ main.py
@@ -12,12 +12,28 @@ SPECIALCHARS = string.punctuation
 ALPHABET = LETTERS+DIGITS+SPECIALCHARS  # Concatnate to create the alphabet
 
 
+def integerLenPassword():
+    failsafe = 0
+    while(True):
+        choice = input("Enter a choice:\n1 create password\n2 Exit\nChoice: ")
+        if(choice == "1"):
+            password = ""
+            passwordLen = int(input("Please enter the length of your password: "))
+            for i in range(passwordLen):
+                password += "".join(secrets.choice(ALPHABET))
+                return password
+        elif(choice == "2"):
+            return "The program is exiting..."
+        elif(failsafe == 2):
+            return "The failsafe has engaged. Exiting..."
+        else:
+            print("\nThe value entered is invalid\n")
+            failsafe += 1
+
+
+
 def main():
-    pwdLength = int(input("Please input an integer: "))
-    password = ""
-    for i in range(pwdLength):
-        password += "".join(secrets.choice(ALPHABET))
-    print(password)
+    print(integerLenPassword())
 
 
 main()
