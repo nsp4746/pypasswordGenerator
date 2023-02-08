f7b6bdb7ebaade3a699012eb3c4673e0d4dd6535
nsp4746 <nsp4746@rit.edu>
2023-02-07 23:01:57 -0500

    GUI Completed and Functional

--- gui.py
+++ gui.py
@@ -8,47 +8,61 @@ from generator import *
 # GUI for python password generator
 
 def PopUp():
+    '''
+    Was originally supposed to be the first thing a user sees when they open the program
+    Not utilized due it altering functionality
+    '''
     messagebox.showinfo("Welcome!","Hi! Welcome to the password generator, this was just a passion project, so its functionality is quite limited (for now)\nThanks for checking it out! \(^-^)/")
 
-def prompt():
-    messagebox.showinfo("New Notification","Click okay to proceed")
-
-
 def createCanvas(window, width, height):
+    '''
+    Function to create a canvas, takes parameters for the window, width, and the height
+    '''
     canvas = tk.Canvas(window,width=width,height=height)
     return canvas
 
-
-
-
-
 def intialize():
+    '''
+    Intializes and creates the GUI 
+    '''
     window = tk.Tk()
     window.geometry("360x360")
     
     canvas = createCanvas(window,360,360)
     canvas.pack()
-    
+
+
+    welcomeLabel = tk.Label(window, text="Hi! Welcome to the password generator, this was just a passion project, so its functionality is quite limited (for now)\nThanks for checking it out! \(^-^)/", wraplength=150)
+    canvas.create_window(180,50,window=welcomeLabel)
+
+  
     entry = tk.Entry(window)
     canvas.create_window(180,300,window=entry)
-    length = entry.get()
 
+    def getEntry():
+        '''
+        Function to retrieve the information the user input into the entry box
+        '''
+        entryValue = entry.get()
+        return entryValue
+    
     def genPassword():
-        label = tk.Label(window, text=gui_integerLenPassword(length))
+        '''
+        This function is called whenever a user presses the button, to generate their password and present it on a label
+        '''
+        label = tk.Label(window, text=gui_integerLenPassword(getEntry()))
         canvas.create_window(180,240, window=label)
         
     button = tk.Button(text="Generate Password", command=genPassword)
 
     canvas.create_window(180,180,window=button)
 
-    #PopUp()
+    entry.focus()
     window.mainloop()
-    
-def GUI():
-    intialize()
+
 
 def main():
-    GUI()
+    intialize()
 
 if __name__ == "__main__":
     main()
